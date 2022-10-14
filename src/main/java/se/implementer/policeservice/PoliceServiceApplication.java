89d0b439427f530a0fbd76a2ae35f9f6f98b48a6
zpri <zoran.pribojevic@sbab.se>
2022-10-14 21:16:02 +0200

    initial commit

---
+++ src/main/java/se/implementer/policeservice/PoliceServiceApplication.java
@@ -0,0 +1,13 @@
+package se.implementer.policeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoliceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoliceServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/implementer/policeservice/PoliceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package se.implementer.policeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoliceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
