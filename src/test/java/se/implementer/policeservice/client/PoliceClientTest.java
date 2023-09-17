1fc060f0c71748352c99e26eaf188944dc32db08
zpri <zoran.pribojevic@sbab.se>
2023-09-17 19:52:59 +0200

    add test case

--- src/test/java/se/implementer/policeservice/client/PoliceClientTest.java
+++ src/test/java/se/implementer/policeservice/client/PoliceClientTest.java
@@ -37,6 +37,7 @@ public class PoliceClientTest {
         assertThat(response).isNotNull();
         assertEquals(response.size(), 1);
         assertEquals(response.get(0).getLocation().getName(), "Solna");
+        assertEquals(response.get(0).getId(), 375940);
     }
 
 }
