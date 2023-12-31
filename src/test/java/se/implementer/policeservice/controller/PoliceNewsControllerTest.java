db9209aa21ef0a9f27229502f8e38a94c9aa1440
zpri <zoky.zp@gmail.com>
2023-12-31 17:01:41 +0700

    adding component test for alert warning

--- src/test/java/se/implementer/policeservice/controller/PoliceNewsControllerTest.java
+++ src/test/java/se/implementer/policeservice/controller/PoliceNewsControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import se.implementer.policeservice.service.PoliceService;
 
@@ -18,6 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static utils.TestData.createPoliceEvents;
 
 @WebMvcTest(controllers = PoliceNewsController.class)
+@ActiveProfiles("test")
 public class PoliceNewsControllerTest {
 
     @MockBean
