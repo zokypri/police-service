90c8bcd8905f2de74e2880a458f4bca66fe226c5
zpri <zoky.zp@gmail.com>
2024-01-19 20:46:02 +0700

    added validation for callback alert input

--- src/main/java/se/implementer/policeservice/model/kafka/PoliceWarningAlert.java
+++ src/main/java/se/implementer/policeservice/model/kafka/PoliceWarningAlert.java
@@ -1,14 +1,23 @@
 package se.implementer.policeservice.model.kafka;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Value;
 
 @Value
 @Builder
 public class PoliceWarningAlert {
-    //TODO validate input
+
+    @NotNull
+    @Min(value = 1)
     int id;
 
+    @NotNull
+    @NotBlank(message = "Alert message is mandatory")
+    @Size(max = 255, message = "Can't be longer than 255 chars")
     String msg;
 
     WarningLevel warningLevel;
